334047139de1e2141b82792c57849d8d3ba7db61
Mohammad Hafijul Islam <hafijul233@gmail.com>
2026-05-18 15:56:25 +0600

    backup functionally refactored and down issue fixed

--- Http/Controllers/BackupCrudController.php
+++ Http/Controllers/BackupCrudController.php
@@ -3,15 +3,16 @@
 namespace Amplify\System\Utility\Http\Controllers;
 
 use Amplify\System\Abstracts\BackpackCustomCrudController;
-use Amplify\System\Backend\Traits\ZipperTrait;
+use Amplify\System\Utility\Jobs\DbBackupQueueJob;
+use Amplify\System\Utility\Models\ApiLog;
 use Backpack\CRUD\app\Library\CrudPanel\CrudPanelFacade as CRUD;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Artisan;
+use Carbon\Carbon;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Storage;
-use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Illuminate\Support\Number;
+use Prologue\Alerts\Facades\Alert;
 use Symfony\Component\HttpFoundation\StreamedResponse;
 
 /**
@@ -23,7 +24,6 @@ class BackupCrudController extends BackpackCustomCrudController
 {
     use \Backpack\CRUD\app\Http\Controllers\Operations\DeleteOperation;
     use \Backpack\CRUD\app\Http\Controllers\Operations\ListOperation;
-    use ZipperTrait;
 
     /**
      * Configure the CrudPanel object. Apply settings to all operations.
@@ -32,19 +32,23 @@ class BackupCrudController extends BackpackCustomCrudController
      */
     public function setup()
     {
-        CRUD::setRoute(config('backpack.base.route_prefix').'/backup');
+        CRUD::setModel(ApiLog::class);
+        CRUD::setRoute(config('backpack.base.route_prefix') . '/backup');
         CRUD::setEntityNameStrings('backup', 'backups');
     }
 
     protected function setupCustomRoutes($segment, $routeName, $controller)
     {
-        Route::post($segment.'/create', [
-            'as' => $routeName.'.create',
-            'uses' => $controller.'@create',
+        Route::get("{$segment}/create", [
+            'as' => "{$routeName}.create",
+            'uses' => "{$controller}@create",
+            'operation' => 'create',
         ]);
-        Route::get($segment.'/download/{filename?}', [
-            'as' => $routeName.'.download',
-            'uses' => $controller.'@download',
+
+        Route::get("{$segment}/{filename}/download", [
+            'as' => "{$routeName}.download",
+            'uses' => "{$controller}@download",
+            'operation' => 'download',
         ]);
     }
 
@@ -57,139 +61,186 @@ class BackupCrudController extends BackpackCustomCrudController
      */
     protected function setupListOperation()
     {
-        $this->data['backups'] = [];
+        $this->crud->addButton('line', 'download', 'view', 'backend::buttons.backup_download', 'beginning');
 
-        $this->crud->setHeading('Backups');
-
-        foreach (config('backup.backup.destination.disks') as $disk_name) {
-            $disk = Storage::disk($disk_name);
-            $can_download = backpack_user()->can('backup.download');
-            $files = $disk->allFiles();
-
-            // make an array of backup files, with their filesize and creation date
-            foreach ($files as $k => $f) {
-                // only take the zip files into account
-                if (substr($f, -4) == '.zip' && $disk->exists($f)) {
-                    $this->data['backups'][] = [
-                        'file_path' => $f,
-                        'file_name' => basename($f),
-                        'file_size' => $disk->size($f),
-                        'last_modified' => $disk->lastModified($f),
-                        'disk' => $disk_name,
-                        'download' => $can_download,
-                    ];
-                }
-            }
-        }
-
-        $this->data['backups'] = array_reverse($this->data['backups']);
-
-        $this->crud->setSubheading('Showing all entries on backups disks');
-
-        $this->crud->setListView('backend::pages.backup.list');
+        CRUD::addColumns([
+            [
+                'name' => 'name',
+                'label' => 'File',
+            ],
+            [
+                'name' => 'size',
+                'label' => 'Size',
+                'type' => 'text',
+                'orderable' => false,
+            ],
+            [
+                'name' => 'modified',
+                'label' => 'Created',
+                'type' => 'datetime',
+                'orderable' => false,
+            ],
+        ]);
     }
 
-    public function create(Request $request): \Illuminate\Http\Response|string
+    /**
+     * The search function that is called by the data table.
+     *
+     * @return array JSON Array of cells in HTML form.
+     */
+    public function search()
     {
-        $message = 'success';
-        $objects = $request->input('objects');
+        $this->crud->hasAccessOrFail('list');
 
-        if (empty($objects)) {
-            return Response::make('Please select at least one object.', 500);
+        $disk = Storage::disk('backups');
+
+        $files = $disk->allFiles();
+
+        $entries = collect();
+
+        foreach ($files as $index => $f) {
+            if (str_ends_with($f, '.zip') && $disk->exists($f)) {
+                $entries->push(new class($index, $f, $disk) {
+                    public $index;
+                    public string $id;
+                    public string $name;
+                    public string $size;
+                    public $modified;
+
+                    public function __construct($pos, $f, $disk)
+                    {
+                        $this->id = base64_encode(basename($f));
+                        $this->index = $pos + 1;
+                        $this->name = ucfirst(basename($f));
+                        $this->size = Number::fileSize($disk->size($f));
+                        $this->modified = Carbon::createFromTimestamp($disk->lastModified($f));
+                    }
+
+                    public function getKey()
+                    {
+                        return $this->id;
+                    }
+                });
+            }
         }
+
+        $entries = $entries->sortByDesc('modified');
+
+        $totalEntryCount = $entries->count();
+
+        $search = request()->input('search');
+        $start = request()->input('start', 0);
+        $length = request()->input('length', 10);
+
+        // if a search term was present
+        if ($search && $search['value'] ?? false) {
+            $entries = $entries->filter(function ($entry) use ($search) {
+                return str_contains($entry->name, $search['value']);
+            });
+        }
+
+        // start the results according to the datatables pagination
+        if ($start) {
+            $entries = $entries->skip($start);
+        }
+        // limit the number of results according to the datatables pagination
+        if ($length) {
+            $entries = $entries->take($length);
+        }
+
+        // if show entry count is disabled we use the "simplePagination" technique to move between pages.
+        if ($this->crud->getOperationSetting('showEntryCount')) {
+            $filteredEntryCount = $this->crud->getFilteredQueryCount() ?? $totalEntryCount;
+        } else {
+            $totalEntryCount = $length;
+            $filteredEntryCount = $entries->count() < $length ? 0 : $length + $start + 1;
+        }
+
+        // store the totalEntryCount in CrudPanel so that multiple blade files can access it
+        $this->crud->setOperationSetting('totalEntryCount', $totalEntryCount);
+
+        return $this->crud->getEntriesAsJsonForDatatables($entries->toArray(), $totalEntryCount, $filteredEntryCount, $start);
+    }
+
+    public function create(): RedirectResponse
+    {
+        $this->crud->hasAccessOrFail('create');
 
         try {
-            ini_set('max_execution_time', 600);
 
-            Log::info('Backpack\BackupManager -- Called backup:run from admin interface');
+            if (!app()->isProduction()) {
 
-            if (in_array('full_backup', $objects)) {
-                Artisan::call('backup:run --disable-notifications');
-            } elseif (in_array('database', $objects)) {
-                Artisan::call('backup:run --only-db --disable-notifications');
+                Alert::error('<strong>Backup Process Failed</strong><br>The Backup process can only be run in production environment')->flash();
+
+                return redirect()->back();
             }
 
-            $output = Artisan::output();
-            if (strpos($output, 'Backup failed because')) {
-                preg_match('/Backup failed because(.*?)$/ms', $output, $match);
-                $message = "Backpack\BackupManager -- backup process failed because ";
-                $message .= isset($match[1]) ? $match[1] : '';
-                Log::error($message.PHP_EOL.$output);
-            } else {
-                Log::info("Backpack\BackupManager -- backup process has started");
-            }
+            DbBackupQueueJob::dispatch();
 
-            if (in_array('storage', $objects) && ! in_array('full_backup', $objects)) {
-                $folders = ['storage', 'uploads'];
+            Alert::success('<strong>Backup Process Started</strong><br>The backup file will be available for download once the process is complete')->flash();
 
-                foreach ($folders as $folder) {
-                    $folderPath = public_path($folder);
-                    $zipFileName = time().'-'.$folder.'-backup-'.date('Y-m-d').'.zip';
-                    $zipResult = $this->takeStorageBackup($folderPath, $zipFileName);
-
-                    if ($zipResult != 'success') {
-                        $message = $zipResult;
-                        Log::error('BackUp of '.$folder.' failed. Error:'.$message);
-                    }
-                }
-            }
         } catch (\Exception $e) {
+
             Log::error($e);
 
-            return Response::make($e->getMessage(), 500);
+            Alert::error('Failed to start backup process. Please try again.')->flash();
         }
 
-        return $message;
+        return redirect()->back();
+
     }
 
     /**
      * Downloads a backup zip file.
      *
-     * @return BinaryFileResponse|StreamedResponse|void
+     * @return StreamedResponse|RedirectResponse
      */
-    public function download(Request $request)
+    public function download($filename)
     {
-        $disk_name = $request->input('disk');
-        $disk = Storage::disk($disk_name);
-        $file_name = $request->input('path');
-        $can_download = backpack_user()->can('backup.download');
+        $this->crud->hasAccessOrFail('download');
 
-        if ($can_download) {
-            if ($disk->exists($file_name)) {
-                if (method_exists($disk->getAdapter(), 'getPathPrefix')) {
-                    $storage_path = $disk->getAdapter()->getPathPrefix();
+        $filename = base64_decode($filename);
 
-                    return response()->download($storage_path.$file_name);
-                } else {
-                    return $disk->download($file_name);
-                }
-            } else {
-                abort(404, trans('backpack::backup.backup_doesnt_exist'));
-            }
-        } else {
-            abort(403);
-        }
-    }
+        $disk = Storage::disk('backups');
 
-    /**
-     * Deletes a backup file.
-     */
-    public function destroy($file_name, Request $request): string
-    {
-        $diskName = $request->input('disk', 'backups');
-
-        if (! in_array($diskName, config('backup.backup.destination.disks'))) {
-            abort(500, trans('backpack::backup.unknown_disk'));
-        }
-
-        $disk = Storage::disk($diskName);
-
-        if ($disk->exists(config('backup.backup.name').'/'.$file_name)) {
-            $disk->delete(config('backup.backup.name').'/'.$file_name);
-
-            return 'success';
-        } else {
+        if (!$disk->exists($filename)) {
             abort(404, trans('backpack::backup.backup_doesnt_exist'));
         }
+
+        // S3 or S3-compatible disks
+        if (in_array($disk->getConfig()['driver'], ['s3'])) {
+
+            return redirect()->away(str_replace('\\', '/', $disk->url($filename)));
+        }
+
+        // Local disk
+        return response()->streamDownload(function () use ($disk, $filename) {
+
+            $stream = $disk->readStream($filename);
+
+            fpassthru($stream);
+
+            if (is_resource($stream)) {
+                fclose($stream);
+            }
+
+        }, $filename, [
+            'Content-Type' => 'application/octet-stream',
+        ]);
+    }
+
+    public function destroy($id)
+    {
+        $this->crud->hasAccessOrFail('delete');
+
+        $filename = base64_decode($id);
+
+        $disk = Storage::disk('backups');
+
+        if (!$disk->exists($filename)) {
+            abort(404, trans('backpack::backup.backup_doesnt_exist'));
+        }
+
+        return $disk->delete($filename);
     }
 }
---
+++ Jobs/DbBackupQueueJob.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Amplify\System\Utility\Jobs;
+
+use Amplify\System\Backend\Commands\BackupRunCommand;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Artisan;
+
+class DbBackupQueueJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        Artisan::call(BackupRunCommand::class);
+    }
+}
